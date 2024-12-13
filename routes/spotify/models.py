2b7b34918170d85057db8d67b2c60048da3f9751
LUXTACO <tacomastabusiness@gmail.com>
2024-12-13 14:32:54 -0600

    Enhance Spotify models with additional attributes for albums, tracks, and playlists

--- routes/spotify/models.py
+++ routes/spotify/models.py
@@ -9,6 +9,7 @@ class Artist:
     monthly_listeners: int
     listeners_location: dict
     popular_tracks: List[mTrack] #? List of popular tracks, normally 5 unless user specifies otherwise
+    popular_discography: List[mAlbum] #? List of albums normally 5 unless user specifies otherwise
 
 class Track:
     id_: str
@@ -16,37 +17,38 @@ class Track:
     name: str
     streams: int
     album: mAlbum
-    artists: List[mArtist]
     release_year: str
+    artists: List[mArtist]
+    recommendations: List[mTrack]
     
 class Album: 
     id_: str
     url: str
     name: str
-    artists: List[mArtist]
-    tracks: List[mTrack]
-    release_year: str
     duration: int
+    release_year: str
+    tracks: List[mTrack]
+    artists: List[mArtist]
     
 class Playlist:
     id_: str
     url: str
     name: str
     duration: int
+    description: str
     saved_times: int
     authors: List[mArtist]
-    description: str
     
 #% Minified version of the track class 
 class mTrack: 
     id_: str
     url: str
     name: str
-    streams: int
     artists: List[mArtist]
     duration: int #? Should be in seconds
     
     def add_attribute(self, key: str, value: str):
+        #> added date | album | streams
         setattr(self, key, value)
     
 #% Minified version of the artist class
@@ -73,6 +75,8 @@ class mAlbum:
     id_: str
     url: str
     name: str
+    is_single: bool
+    release_year: str
     
     def add_attribute(self, key: str, value: str):
         setattr(self, key, value)
