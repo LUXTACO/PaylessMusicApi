033d744db4df59dd00324b006331c5638f835d87
LUXTACO <tacomastabusiness@gmail.com>
2024-12-13 16:55:47 -0600

    Add new API routes for playlists, artists, and albums; enhance models with dynamic attributes

--- routes/spotify/__init__.py
+++ routes/spotify/__init__.py
@@ -1,5 +1,6 @@
 import bs4
 import logging
+import requests
 from .models import *
 from fastapi import APIRouter, Depends, Request
 
@@ -14,10 +15,14 @@ router = APIRouter(
 def track(id_: str):
     pass
 
-@router.get()
-def playlist(id_: str):
+@router.get("/playlist", response_model=Playlist)
+def playlist(id_: str, track_count: int = 99):
     pass
 
-@router.get()
-def artist(id_: str):
+@router.get("/artist", response_model=Artist)
+def artist(id_: str, pt_count: int = 5, pd_count: int = 5):
+    pass
+
+@router.get("/album", response_model=Album)
+def album(id_: str):
     pass
--- routes/spotify/models.py
+++ routes/spotify/models.py
@@ -8,8 +8,11 @@ class Artist:
     is_verified: bool
     monthly_listeners: int
     listeners_location: dict
-    popular_tracks: List[mTrack] #? List of popular tracks, normally 5 unless user specifies otherwise
-    popular_discography: List[mAlbum] #? List of albums normally 5 unless user specifies otherwise
+    
+    def add_attribute(self, key: str, value: str):
+        #> popular_tracks | popular_discography
+        #? List of popular |albums/tracks| normally 5 unless user specifies otherwise
+        setattr(self, key, value)
 
 class Track:
     id_: str
@@ -17,18 +20,27 @@ class Track:
     name: str
     streams: int
     album: mAlbum
-    release_year: str
+    release_date: str
     artists: List[mArtist]
-    recommendations: List[mTrack]
+    
+    def add_attribute(self, key: str, value: str):
+        #> recommendations | artist_popular_tracks | artist_popular_albums | artist_popular_posts
+        #? List of recommended |albums/tracks/posts/recommendations|, normally 5 unless user specifies otherwise
+        setattr(self, key, value)
     
 class Album: 
     id_: str
     url: str
     name: str
     duration: int
-    release_year: str
+    release_date: str
     tracks: List[mTrack]
     artists: List[mArtist]
+    
+    def add_attribute(self, key: str, value: str):
+        #> artist_other_albums
+        #? List of other albums by the artist, normally 5 unless user specifies otherwise
+        setattr(self, key, value)
     
 class Playlist:
     id_: str
@@ -39,6 +51,11 @@ class Playlist:
     saved_times: int
     authors: List[mArtist]
     
+    def add_attribute(self, key: str, value: str):
+        #> similar_content
+        #? List of similar content, normally 5 unless user specifies otherwise
+        setattr(self, key, value)
+    
 #% Minified version of the track class 
 class mTrack: 
     id_: str
@@ -48,7 +65,8 @@ class mTrack:
     duration: int #? Should be in seconds
     
     def add_attribute(self, key: str, value: str):
-        #> added date | album | streams
+        #> added_date | album | streams
+        #? added_date is the date the track was added to the playlist, album is the album the track belongs to, streams is the number of streams the track has
         setattr(self, key, value)
     
 #% Minified version of the artist class
@@ -75,8 +93,8 @@ class mAlbum:
     id_: str
     url: str
     name: str
-    is_single: bool
-    release_year: str
     
     def add_attribute(self, key: str, value: str):
+        #> is_single | release_year
+        #? is_single is a boolean value, release_year is the year the album was released
         setattr(self, key, value)
