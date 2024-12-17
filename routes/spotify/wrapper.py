f0aeaa78e1d9a2e1885bbcf87f0142790db8fe28
LUXTACO <tacomastabusiness@gmail.com>
2024-12-17 01:19:39 -0600

    Refactor Wrapper class to streamline token management; add methods for track retrieval and token validation

--- routes/spotify/wrapper.py
+++ routes/spotify/wrapper.py
@@ -1,29 +1,82 @@
 import bs4
 import time
+import json
 import logging
 import datetime
 import requests
+import urllib.parse
 
 logger = logging.getLogger(__name__)
 
 class Wrapper:
     def __init__(self):
-        self.session = requests.Session()
-        
         self.client_id = None
         self.access_token = None
         self.access_token_timestamp = None
         self.client_token = None
         self.client_token_timestamp = None
-        
-        self.get_access_token()
-        self.get_client_token()
-        
-    def get_client_token(self):
-        while self.client_token == None:
-            response = self.session.get("https://clienttoken.spotify.com/v1/clienttoken", headers={"Accept": "application/json"}, payload={
-                    "client_data": {
-                    "client_version": "1.2.54.42.gd1ce9e40", #? Update this if needed
+
+    #> Public methods
+    def get_track(self, track_id: str) -> bool|dict:
+        logger.info("Getting track from Spotify...")
+        response = requests.get(("https://api-partner.spotify.com/pathfinder/v1/query?" + urllib.parse.urlencode({
+            "operationName": "getTrack",
+            "variables": json.dumps({"uri": f"spotify:track:{track_id}"}),
+            "extensions": json.dumps({"persistedQuery": {"version": 1, "sha256Hash": "5c5ec8c973a0ac2d5b38d7064056c45103c5a062ee12b62ce683ab397b5fbe7d"}})
+        })), headers=self.get_headers())
+        if response.status_code == 200:
+            logger.info("Successfully retrieved track from Spotify.")
+            return response.json()
+        else:
+            logger.error(f"Failed to retrieve track from Spotify. Status code: {response.status_code}")
+            return False
+    
+    def get_headers(self):
+        self._update_tokens()
+        return {"Accept": "*/*", "Authorization": f"Bearer {self.access_token}", "Client-Token": self.client_token}
+    
+    #> Private methods
+    def _update_tokens(self):
+        if self.access_token is None or self.client_token is None:
+            self._get_access_token()
+            self._get_client_token()
+        else:
+            if (datetime.datetime.now() - self.access_token_timestamp).seconds > 3600:
+                self._get_access_token()
+            if (datetime.datetime.now() - self.client_token_timestamp).seconds > 3600:
+                self._get_client_token()
+    
+    def _get_access_token(self):
+        while self.access_token is None:
+            logger.info("Requesting access token from Spotify...")
+            response = requests.get("https://open.spotify.com", headers={"Accept": "text/html"})
+            if response.status_code == 200:
+                logger.info("Successfully connected to Spotify.")
+                soup = bs4.BeautifulSoup(response.text, "html.parser")
+                script_tag = soup.find("script", {"id": "session", "data-testid": "session"})
+                if script_tag:
+                    logger.info("Found session script tag.")
+                    session_data = script_tag.string
+                    session_json = json.loads(session_data)
+                    self.access_token = session_json.get("accessToken")
+                    self.access_token_timestamp = datetime.datetime.now()
+                    self.client_id = session_json.get("clientId")
+                    logger.info("Access token and client ID retrieved successfully.")
+                    logger.debug(f"Access token: {self.access_token}")
+                    logger.debug(f"Client ID: {self.client_id}")
+                else:
+                    logger.error("Session script tag not found. Retrying in 2 seconds...")
+                    time.sleep(2)
+            else:
+                logger.error(f"Failed to connect to Spotify. Status code: {response.status_code}. Retrying in 2 seconds...")
+                time.sleep(2)
+    
+    def _get_client_token(self):
+        while self.client_token is None:
+            logger.info("Requesting client token from Spotify...")
+            response = requests.post("https://clienttoken.spotify.com/v1/clienttoken", headers={"Accept": "application/json"}, json={
+                "client_data": {
+                    "client_version": "1.2.54.42.gd1ce9e40",  #? Update this if needed
                     "client_id": self.client_id,
                     "js_sdk_data": {
                         "device_brand": "unknown",
@@ -35,53 +88,22 @@ class Wrapper:
                     }
                 }
             })
-            if response.status_code() == 200:
-                logger.info("Client token request went through")
-                parsed_response = response.json()
-                if parsed_response["response_type"] == "RESPONSE_GRANTED_TOKEN_RESPONSE":
-                    self.client_token = parsed_response["granted_token"]["token"]
-                    self.client_token_timestamp = datetime.datetime.now()
-                else:
-                    logger.error(f"Client token request was denied by the server, status code: {response.status_code}")
-                    time.sleep(2)
-                    continue
-                
-    def get_access_token(self):
-        while self.access_token == None:
-            response = self.session.get("https://open.spotify.com", headers={"Accept": "text/html"})
             if response.status_code == 200:
-                logger.info("Auth token request went through")
-                soup = bs4.BeautifulSoup(response.text, "html.parser")
-                script_tag = soup.find("script", {"id": "session", "data-testid": "session"})
-                if script_tag:
-                    session_data = script_tag.string
-                    session_json = json.loads(session_data)
-                    self.access_token = session_json.get("accessToken")
-                    self.access_token_timestamp = datetime.datetime.now()
-                    self.client_id = session_json.get("clientId")
-                    logger.info("Access token retrieved successfully")
+                parsed_response = response.json()
+                if parsed_response.get("response_type") == "RESPONSE_GRANTED_TOKEN_RESPONSE":
+                    logger.info("Client token request was granted by the server.")
+                    self.client_token = parsed_response.get("granted_token").get("token")
+                    self.client_token_timestamp = datetime.datetime.now()
+                    logger.debug(f"Client token: {self.client_token}")
                 else:
-                    logger.error("Session script tag not found, trying again")
-                    continue
+                    logger.error(f"Client token request was denied by the server, status code: `{response.status_code}`")
+                    time.sleep(2)
             else:
-                logger.error(f"Failed to get auth token, status code: {response.status_code}")
+                logger.error(f"Failed to retrieve client token. Status code: {response.status_code}. Retrying in 2 seconds...")
                 time.sleep(2)
-                continue
-                
-    def check_client_token(self):
-        if self.client_token == None:
-            self.get_client_token()
-        else:
-            if (datetime.datetime.now() - self.client_token_timestamp).seconds > 1209600:
-                self.get_client_token()
-            else:
-                return True
-            
-    def check_access_token(self):
-        if self.access_token == None:
-            self.get_access_token()
-        else:
-            if (datetime.datetime.now() - self.access_token_timestamp).seconds > 3600:
-                self.get_access_token()
-            else:
-                return True
+
+#? Unit test
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+    wrapper = Wrapper()
+    print(wrapper.get_track("68IdCDcjQGPD2OjJ5l6KQL"))
