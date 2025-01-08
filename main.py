1d416bc2bda5efcc1e331ec3fe0139db183b2250
LUXTACO <tacomastabusiness@gmail.com>
2025-01-08 01:07:14 -0600

    Add comment to clarify purpose of main execution block for local testing

--- main.py
+++ main.py
@@ -33,5 +33,5 @@ def health(request: fastapi.Request):
     logger.info(f"Health check invoked by {request.client.host}")
     return fastapi.responses.JSONResponse(content={"status": "OK", "up_since": init_time}, status_code=200)
 
-if __name__ == "__main__":
+if __name__ == "__main__": #? Used for local testing
     uvicorn.run(app, host="0.0.0.0", port=8000)
