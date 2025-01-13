39f45942e3d5a77d5d2af468227ca8e213d9a7b6
LUXTACO <tacomastabusiness@gmail.com>
2025-01-12 23:59:23 -0600

    Update Spotify wrapper to return artistUnion from playlist response

--- routes/spotify/wrapper.py
+++ routes/spotify/wrapper.py
@@ -42,7 +42,7 @@ class Wrapper:
         })), headers=self.get_headers())
         if response.status_code == 200:
             logger.info("Successfully retrieved playlist from Spotify.")
-            return response.json()["data"]
+            return response.json()["data"]["artistUnion"]
         else:
             logger.error(f"Failed to retrieve playlist from Spotify. Status code: {response.status_code}")
             return False
